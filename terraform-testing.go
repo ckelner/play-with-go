fb49ba83b7485d1312c803862619544ce91d8773
ckelner <chris.kelner@gmail.com>
2015-09-10 11:55:11 -0400

    example of empty int producing zero through conversion

---
+++ terraform-testing.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+import "github.com/aws/aws-sdk-go/aws"
+
+func main() {
+  var cookie_expire int
+  fmt.Println(int64(cookie_expire))
+  fmt.Println(aws.Int64(int64(cookie_expire)))
+}
